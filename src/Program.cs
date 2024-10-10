5f6e01455bbb89c954f7930c3bcbb6ddd364d1dd
mshogren <3902009+mshogren@users.noreply.github.com>
2024-10-10 13:34:43 -0600

    Added code to retrieve albums from database

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,7 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder();
 
 // Add services to the container.
@@ -36,20 +40,57 @@ app.MapGet("/albums", () =>
 })
 .WithName("GetAlbums");
 
+app.MapGet("/albumsdb", async () =>
+{
+    return await Album.GetAllFromDb();
+})
+.WithName("GetAlbumsFromDb");
+
 app.Run();
 
-record Album(int Id, string Title, string Artist, double Price, string Image_url)
+record Album(string Id, string Title, string Artist, double Price, string Image_url)
 {
      public static List<Album> GetAll(){
          var albums = new List<Album>(){
-            new Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
-            new Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
-            new Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
-            new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
-            new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            new Album("1", "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
+            new Album("2", "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
+            new Album("3", "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
+            new Album("4", "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
+            new Album("5", "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
+            new Album("6", "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
          };
 
         return albums; 
+     }
+
+     public static async Task<List<Album>> GetAllFromDb() {
+        TokenCredential credential = new DefaultAzureCredential();
+        using CosmosClient client = new(
+            accountEndpoint: Environment.GetEnvironmentVariable("AZURE_COSMOS_RESOURCEENDPOINT")!,
+            tokenCredential: credential
+        );
+        var container = client.GetContainer("main", "albums");
+
+        var query = new QueryDefinition(
+            query: "SELECT * FROM albums"
+        );
+
+        using FeedIterator<Album> feed = container.GetItemQueryIterator<Album>(
+            queryDefinition: query
+        );
+        
+        List<Album> items = new();
+        double requestCharge = 0d;
+        while (feed.HasMoreResults)
+        {
+            FeedResponse<Album> response = await feed.ReadNextAsync();
+            foreach (Album item in response)
+            {
+                items.Add(item);
+            }
+            requestCharge += response.RequestCharge;
+        }
+
+        return items;
      }
 }
